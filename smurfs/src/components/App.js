d8316166d089e1b0456fed401bab2bd87ac86a05
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-25 17:22:36 -0700

    Using state to render info on App component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,15 +1,36 @@
 import React from "react";
+import {connect} from "react-redux";
 import "./App.css";
 
-function App() {
+//context
+
+//components
+
+function App ({village, isLoading, error}) {
    return (
       <div className="App">
          <h1>SMURFS! 2.0 W/ Redux</h1>
          <div>Welcome to your state management version of Smurfs!</div>
          <div>Start inside of your `src/index.js` file!</div>
          <div>Have fun!</div>
+
+         {error && <div>{error}</div>}
+
+         {
+            (!isLoading && village && village.length > 0)
+            ?  <p>Display the village, population: ${village.length}</p>
+            :  <p>Loading...</p>
+         }
       </div>
    );
 }
 
-export default App;
+const mapStateToProps = state => {
+   return {
+      village: state.smurfs.village,
+      isLoading: state.smurfs.isLoading,
+      error: state.smurfs.error
+   };
+};
+
+export default connect(mapStateToProps)(App);
