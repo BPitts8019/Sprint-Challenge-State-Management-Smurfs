ee8aa193531f911974e8302913015b344a92ee2d
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-25 17:33:27 -0700

    Completed reducer for the village

--- smurfs/src/store/smurfs/smurfsReducer.js
+++ smurfs/src/store/smurfs/smurfsReducer.js
@@ -4,14 +4,36 @@ import {
    FETCH_VILLAGE_ERROR
 } from "./actionTypes";
 
+const NO_ERRORS = null;
+const ZERO_POP = null;
 const initialState = {
-   village: null,
+   village: ZERO_POP,
    isLoading: false,
-   error: null
+   error: NO_ERRORS
 }
 
 export default (state = initialState, action) => {
    switch (action.type) {
+      case FETCH_VILLAGE_START:
+         return {
+            ...state,
+            isLoading: true,
+            error: NO_ERRORS
+         };
+      case FETCH_VILLAGE_END:
+         return {
+            ...state,
+            village: action.payload,
+            isLoading: false,
+            error: NO_ERRORS
+         };
+      case FETCH_VILLAGE_ERROR:
+         return {
+            ...state,
+            village: ZERO_POP,
+            isLoading: false,
+            error: action.payload
+         };
       default:
          return state;
    }
