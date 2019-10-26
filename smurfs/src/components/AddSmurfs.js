9294f7df71128cf77dfbb6c969f3a4de71fb3526
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-25 18:57:47 -0700

    Form is working

--- smurfs/src/components/AddSmurfs.js
+++ smurfs/src/components/AddSmurfs.js
@@ -1,27 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import {connect} from "react-redux";
 
 //context
 import {createSmurf} from "../store/smurfs/villageActions";
 
 function AddSmurfs ({createSmurf}) {
+   const [info, setInfo] = useState({
+      name: "",
+      height: 0.1,
+      age: 0
+   });
+
+
+   const handleChange = event => {
+      const newData = {
+         ...info,
+      };
+
+      newData[event.target.dataset.type] = event.target.value;
+      setInfo(newData);
+   };
+
    const handleClick = event => {
       event.preventDefault();
-      createSmurf();
+      console.log(`Create a Smurf!! ${JSON.stringify(info, null, 3)}`);
    };
+
    return (
       <form name="addSmurfs">
          <label>
             Name:
-            <input type="text" />
+            <input data-type="name" type="text" onChange={handleChange} value={info.name} />
          </label>
          <label>
             Height:
-            <input type="number" min="0.1" placeholder="in centimeters" />
+            <input data-type="height" type="number" min="0.1" onChange={handleChange} value={info.height} />
          </label>
          <label>
             Age: 
-            <input type="number" min="0" />
+            <input data-type="age" type="number" min="0" onChange={handleChange} value={info.age} />
          </label>
          <button type="submit" onClick={handleClick}>Add Smurf</button>
       </form>
